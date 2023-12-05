e3063bea3634b40a90532a823967322dbf1a7fa4
HawkingHuang <math0915@gmail.com>
2023-12-05 19:40:56 +0800

    Add User.vue

---
+++ src/views/User.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <base-card>User</base-card>
+  </div>
+</template>
+
+<script>
+import BaseCard from "@/components/BaseCard.vue";
+export default {
+  components: { BaseCard },
+};
+</script>
+
+<style></style>
